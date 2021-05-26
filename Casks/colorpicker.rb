03c91a385306cae9b1c2165c431b7d980fb8adab
Ryan Nguyen <contactryannguyen@gmail.com>
2021-05-26 14:39:04 -0400

    Add Cask for Colorpicker

---
+++ Casks/colorpicker.rb
@@ -0,0 +1,16 @@
+cask "colorpicker" do
+  version "2.0.5"
+  sha256 "6459d542b4a0786cb9a81e3bec824fe42c3dbdcff48874e7a02a52e143d4a9e8"
+
+  url "https://github.com/Toinane/colorpicker/releases/download/#{version}/Colorpicker-#{version}.dmg",
+      verified: "github.com/Toinane/colorpicker/"
+  name "colorpicker"
+  desc "Minimal but complete colorpicker"
+  homepage "https://colorpicker.fr/"
+
+  app "Colorpicker.app"
+
+  zap trash: [
+    "~/Library/Application Support/Colorpicker"
+  ]
+end
