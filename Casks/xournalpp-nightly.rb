b59816f2456087cd7b9a6e7c47cdb24b9bab8dbe
Ryan Nguyen <contactryannguyen@gmail.com>
2021-06-22 09:42:41 -0400

    Update Xournal++ nightly cask to 20210622

--- Casks/xournalpp-nightly.rb
+++ Casks/xournalpp-nightly.rb
@@ -1,6 +1,6 @@
 cask "xournalpp-nightly" do
-  version "1.1.0.dev-nightly.20210618"
-  sha256 "68c0b3152d875f2ad0b0b4134da96fc98d3b7e02a6aa4d604ac9850ecc62ee46"
+  version "1.1.0.dev-nightly.20210622"
+  sha256 "d490bcddd2e1196f20e354b0b6736d89f93f49d1fb23a16dc068ba613daacfe6"
 
   url "https://github.com/xournalpp/xournalpp/releases/download/nightly/xournalpp-#{version}-macos.zip"
   # url "https://github.com/xournalpp/xournalpp/releases/download/#{version}/xournalpp-#{version}-macos.zip"
