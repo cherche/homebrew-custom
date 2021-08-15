7b8ebf020e09e99b8f7f128cba7c59879e5ff5d3
Ryan Nguyen <contactryannguyen@gmail.com>
2021-08-15 18:21:27 -0400

    Change airshipper Formula GitHub repo to veloren/Airshipper

--- Formula/airshipper-binary.rb
+++ Formula/airshipper-binary.rb
@@ -1,6 +1,6 @@
 class AirshipperBinary < Formula
   desc "Launcher for Veloren voxel game"
-  homepage "https://github.com/Songtronix/Airshipper/"
+  homepage "https://github.com/veloren/Airshipper/"
   version "0.5.0"
   url "https://github.com/veloren/Airshipper/releases/download/v#{version}/airshipper-macos.tar.gz"
   sha256 "43f7de3b252beabb4c8060c792cb63f7f14498a81dcebc2cf68375c38c3fceb1"
