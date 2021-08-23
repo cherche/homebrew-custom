ba5e67edc406fdf3c6901098420c1a38c6dff32e
Ryan Nguyen <contactryannguyen@gmail.com>
2021-08-23 12:19:55 -0400

    Add Formula spicetify-cli

---
+++ Formula/spicetify-cli.rb
@@ -0,0 +1,28 @@
+class SpicetifyCli < Formula
+  desc "Command-line tool to customize Spotify client"
+  homepage "https://github.com/khanhas/spicetify-cli"
+  version "2.6.0"
+  url "https://github.com/khanhas/spicetify-cli/archive/v#{version}.tar.gz"
+  sha256 "20a4ac6048d6369067f9117fe75e5ab696d6f88d1d9e7faf2f9591c965714608"
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath/"dep"
+    buildpath.install
+
+    cd buildpath do
+      system "go", "build", "-o", "spicetify"
+      bin.install "spicetify"
+      cp_r "./globals.d.ts", bin
+      cp_r "./jsHelper", bin
+      cp_r "./Themes", bin
+      cp_r "./Extensions", bin
+      cp_r "./CustomApps", bin
+      cp_r "./css-map.json", bin
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/spicetify -v")
+  end
+end
